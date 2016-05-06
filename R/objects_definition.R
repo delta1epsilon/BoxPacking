f48a4083bf8c0f140d788d7f88db5a8fafdb081c
ikovalyshyn <ikovalyshyn@malkosua.com>
2016-05-06 22:27:42 +0300

    add validity for classes

--- R/objects_definition.R
+++ R/objects_definition.R
@@ -5,7 +5,13 @@ Container <- setClass('Container',
                                 height = 'numeric',
                                 width = 'numeric'
                                 ),
-                      prototype = list(origin = c(0, 0, 0))
+                      prototype = list(origin = c(0, 0, 0)),
+                      validity = function (object) {  # make sure that all parameters are positive
+                              if(object@length <= 0 | object@height <= 0 | object@width <= 0) {
+                                  return("A number <= 0 for one of the parameters was given.")
+                              }
+                              return(TRUE)
+                          }
                       )
 
 # define class for boxes
@@ -15,7 +21,13 @@ Box <- setClass('Box',
                           height = 'numeric',
                           width = 'numeric',
                           weight = 'numeric'
-                          )
+                          ),
+                validity = function (object) {  # make sure that all parameters are positive
+                        if(object@length <= 0 | object@height <= 0 | object@width <= 0) {
+                            return("A number <= 0 for one of the parameters was given.")
+                        }
+                        return(TRUE)
+                    }
                 ) 
 
 # define class for Empty Maximal Spaces
