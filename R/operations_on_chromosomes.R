28c25d7ba712ec321eb277d70ac383d496a26e61
ikovalyshyn <ikovalyshyn@malkosua.com>
2016-05-15 13:45:51 +0300

    add PerformChromosomeSelection

--- R/operations_on_chromosomes.R
+++ R/operations_on_chromosomes.R
@@ -9,3 +9,20 @@ CreateChromosome <- function (n_boxes, n_containers) {
     chromosome <- list(BPS = sample(1:n_boxes), CLS = sample(1:n_containers))
     return(chromosome)
 }
+
+
+
+# TODO: modify PerformChromosomeSelection function to do 
+#       more advanced selection
+
+#' Perform Selection of the best Chromosomes within population 
+#'
+#' @param fitness        - A vector with chromosomes scores   
+#' @param selection_size - An integer
+#' @return A vector of size 'selection_size' with indeces of 
+#'         best performing chromosomes
+PerformChromosomeSelection <- function (fitness, 
+                                        selection_size) {
+    best_chromosomes_ind <- order(fitness)[1:selection_size]
+    return(best_chromosomes_ind)
+}
