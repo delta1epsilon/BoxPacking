c3aade902e683967ec4a9b9dd33f10cee8e6a94b
ikovalyshyn <ikovalyshyn@malkosua.com>
2016-05-17 23:07:22 +0300

    add tests for Custom Initialization

---
+++ tests/test_CustomChromosomeInitialization.R
@@ -0,0 +1,43 @@
+context('CustomChromosomeInitialization')
+
+source('../R/operations_on_chromosomes.R')
+source('../R/objects_definition.R')
+
+b1 <- Box(length = 0.5, height = 0.5, width = 0.5)
+b2 <- Box(length = 1, height = 0.5, width = 0.5)
+b3 <- Box(length = 2, height = 0.5, width = 0.5)
+b4 <- Box(length = 1, height = 1, width = 1)
+
+boxes <- list(b1, b2, b3, b4)
+
+test_that('Test by length',
+          {
+            result <- CustomChromosomeInitialization(boxes, 2, 1)
+            expected <- c(3, 2, 4, 1)
+            expect_equal(result$BPS, expected)
+          }
+         )
+
+test_that('Test by height',
+          {
+            result <- CustomChromosomeInitialization(boxes, 2, 2)
+            expected <- c(4, 1, 2, 3)
+            expect_equal(result$BPS, expected)
+          }
+         )
+
+test_that('Test by width',
+          {
+            result <- CustomChromosomeInitialization(boxes, 2, 3)
+            expected <- c(4, 1, 2, 3)
+            expect_equal(result$BPS, expected)
+          }
+         )
+
+test_that('Test by volume',
+          {
+            result <- CustomChromosomeInitialization(boxes, 2, 4)
+            expected <- c(4, 3, 2, 1)
+            expect_equal(result$BPS, expected)
+          }
+         )
