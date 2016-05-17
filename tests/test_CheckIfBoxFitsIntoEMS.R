0343357fc884a4c79be19bd362e0c0b08d3e0ec1
ikovalyshyn <ikovalyshyn@malkosua.com>
2016-05-17 11:31:15 +0300

    add tests for place selection

---
+++ tests/test_CheckIfBoxFitsIntoEMS.R
@@ -0,0 +1,43 @@
+context('CheckIfBoxFitsIntoEMS')
+
+source('../R/objects_definition.R')
+source('../R/operations_on_boxes.R')
+
+test_that('Test 1',
+          {
+            box <- Box(origin = c(0, 0, 0), length = 2, height = 1, width = 1)
+            ems <- EMS(origin = c(0, 0, 0), length = 1.5, height = 3, width = 2)
+
+            result <- CheckIfBoxFitsIntoEMS(box, ems)
+            expect_equal(result,
+                         TRUE
+                         )
+
+          }
+         )
+
+test_that('Test 2',
+          {
+            box <- Box(origin = c(0, 0, 0), length = 2, height = 1, width = 1)
+            ems <- EMS(origin = c(0, 0, 0), length = 1, height = 2, width = 1)
+
+            result <- CheckIfBoxFitsIntoEMS(box, ems)
+            expect_equal(result,
+                         TRUE
+                         )
+
+          }
+         )
+
+test_that('Test 3',
+          {
+            box <- Box(origin = c(0, 0, 0), length = 2, height = 1, width = 1)
+            ems <- EMS(origin = c(0, 0, 0), length = 1.5, height = 1.5, width = 1)
+
+            result <- CheckIfBoxFitsIntoEMS(box, ems)
+            expect_equal(result,
+                         FALSE
+                         )
+
+          }
+         )
---
+++ tests/test_PerformPlacementSelection.R
@@ -0,0 +1,46 @@
+context('PerformPlacementSelection')
+
+source('../R/objects_definition.R')
+source('../R/operations_on_boxes.R')
+
+test_that('Test 1',
+          {
+            box <- Box(origin = c(0, 0, 0), length = 2, height = 1, width = 1)
+            ems <- EMS(origin = c(0, 0, 0), length = 1.5, height = 3, width = 2)
+
+            result <- PerformPlacementSelection(box, ems)
+            expected <- Box(origin = c(0, 0, 0), length = 1, height = 1, width = 2)
+            expect_equal(result,
+                         expected
+                         )
+
+          }
+         )
+
+test_that('Test 2',
+          {
+            box <- Box(origin = c(0, 0, 0), length = 2, height = 1, width = 1)
+            ems <- EMS(origin = c(0, 0, 0), length = 1, height = 2, width = 1)
+
+            result <- PerformPlacementSelection(box, ems)
+            expected <- Box(origin = c(0, 0, 0), length = 1, height = 2, width = 1)
+            expect_equal(result,
+                         expected
+                         )
+
+          }
+         )
+
+test_that('Test 3',
+          {
+            box <- Box(origin = c(0, 0, 0), length = 2, height = 1, width = 1)
+            ems <- EMS(origin = c(0, 0, 0), length = 2.5, height = 1.5, width = 1)
+
+            result <- PerformPlacementSelection(box, ems)
+            expected <- Box(origin = c(0, 0, 0), length = 2, height = 1, width = 1)
+            expect_equal(result,
+                         expected
+                         )
+
+          }
+         )
