96731cf824918da2efd50d1ed4c1dafba42ec690
ikovalyshyn <ikovalyshyn@malkosua.com>
2016-05-21 12:43:25 +0300

    add chromosomes Initialization, Elitism, Selection and Mutation

--- R/operations_on_chromosomes.R
+++ R/operations_on_chromosomes.R
@@ -13,53 +13,147 @@ CreateChromosome <- function (n_boxes, n_containers) {
 
 #' Create 4 Initial Custom Chromosomes
 #'
-#' @param boxes   - A list of objects of class Box
-#' @param sort_by - An integer (in 1:4) 
-#' @return A chromosome 
-CustomChromosomeInitialization <- function (boxes, n_containers, sort_by) {
-    if (sort_by == 1) {
-        boxes_length <- sapply(boxes, function(x) x@length)
-        
-        # sort descending according to length
-        ind_by_length <- order(boxes_length, decreasing = TRUE)
-        chromosome <- list(BPS = ind_by_length, CLS = sample(1:n_containers))
-    } else if (sort_by == 2) {
-        boxes_height <- sapply(boxes, function(x) x@height)
-    
-        # sort descending according to height
-        ind_by_height <- order(boxes_height, decreasing = TRUE)
-        chromosome <- list(BPS = ind_by_height, CLS = sample(1:n_containers))
-    } else if (sort_by == 3) {
-        boxes_width <- sapply(boxes, function(x) x@width)
-    
-        # sort descending according to width
-        ind_by_width <- order(boxes_width, decreasing = TRUE)
-        chromosome <- list(BPS = ind_by_width, CLS = sample(1:n_containers))
-    } else if (sort_by == 4) {
-        boxes_volume <- sapply(boxes, function(x) x@length * x@height * x@width)
+#' @param boxes - A list of objects of class Box
+#' @return A list of 4 chromosomes 
+CustomChromosomeInitialization <- function (boxes, n_containers) {
+    chromosomes <- list()
 
-        # sort descending according to width
-        ind_by_volume <- order(boxes_volume, decreasing = TRUE)
-        chromosome <- list(BPS = ind_by_volume, CLS = sample(1:n_containers))        
-    } else {
-        stop('The argument sort_by must be in 1:4')
-    }
+    boxes_length <- sapply(boxes, function(x) x@length)
+    boxes_width <- sapply(boxes, function(x) x@width)
+    boxes_height <- sapply(boxes, function(x) x@height)
+    boxes_volume <- sapply(boxes, function(x) x@length * x@height * x@width)
+    
+    # sort descending according to length
+    ind_by_length <- order(boxes_length, decreasing = TRUE)
+    chromosome <- list(BPS = ind_by_length, CLS = sample(1:n_containers))
+    chromosomes <- c(chromosomes, list(chromosome))
 
-    return(chromosome)
+    # sort descending according to height
+    ind_by_height <- order(boxes_height, decreasing = TRUE)
+    chromosome <- list(BPS = ind_by_height, CLS = sample(1:n_containers))
+    chromosomes <- c(chromosomes, list(chromosome))
+
+    # sort descending according to width
+    ind_by_width <- order(boxes_width, decreasing = TRUE)
+    chromosome <- list(BPS = ind_by_width, CLS = sample(1:n_containers))
+    chromosomes <- c(chromosomes, list(chromosome))
+
+    # sort descending according to width
+    ind_by_volume <- order(boxes_volume, decreasing = TRUE)
+    chromosome <- list(BPS = ind_by_volume, CLS = sample(1:n_containers))
+    chromosomes <- c(chromosomes, list(chromosome))
+
+    return(chromosomes)
 }
 
 
-# TODO: modify PerformChromosomeSelection function to do 
-#       more advanced selection
+#' Initialize a Population
+#'
+#' @param population_size - An integer
+#' @param n_containers    - An integer
+#' @param boxes           - A list of objects of class Box
+#' @return A list of chromosomes 
+InitializePopulation <- function (population_size, 
+                                  n_containers, 
+                                  boxes) {
+    # get number of boxes
+    n_boxes <- length(boxes)
 
-#' Perform Selection of the best Chromosomes within population 
+    # create 4 custom chromosomes
+    population <- 
+        CustomChromosomeInitialization(boxes = boxes, 
+                                       n_containers = n_containers
+                                       )
+
+    if (population_size <= 4) {
+        population <- population[1:population_size]
+    } else {
+        n_chromosomes_to_create <- population_size - 4
+        for (i in 1:n_chromosomes_to_create) {
+            chromosome <- CreateChromosome(n_boxes = n_boxes, n_containers = n_containers)
+            population <- c(population, list(chromosome))
+        }
+    }
+
+    return(population)
+}
+
+
+#' Perform Elitism: Select the best Chromosomes within population 
 #'
 #' @param fitness        - A vector with chromosomes scores   
-#' @param selection_size - An integer
-#' @return A vector of size 'selection_size' with indeces of 
+#' @param elitism_size - An integer
+#' @return A vector of size 'elitism_size' with indeces of 
 #'         best performing chromosomes
-PerformChromosomeSelection <- function (fitness, 
-                                        selection_size) {
-    best_chromosomes_ind <- order(fitness)[1:selection_size]
+PerformElitism <- function (fitness, 
+                            elitism_size) {
+    best_chromosomes_ind <- order(fitness)[1:elitism_size]
     return(best_chromosomes_ind)
+}
+
+
+#' Perform Selection
+#'
+#' @param population - A list of chromosomes
+#' @param fitness    - A vector
+#' @return A list of chromosomes of the same size as population
+PerformSelection <- function (population, fitness) {
+    population_size <- length(population)
+    new_population <- list()
+
+    for (i in 1:population_size) {
+        chromosomes_ind <- sample(1:population_size, 2)
+        better_chromosome_ind <- which.min(fitness[chromosomes_ind])
+        new_population <- c(new_population, population[chromosomes_ind[better_chromosome_ind]])
+    }
+
+    return(new_population)
+}
+
+
+#' Perform Mutation
+#'
+#' @param population    - A list of chromosomes
+#' @param mutation_prob - A numeric in [0; 1]; A probability for chromosome mutation  
+#' @return A list of chromosomes
+PerformMutation <- function (population, mutation_prob) {
+
+    #' Mutate a chromosome
+    #'
+    #' @param chromosome - A list with BPS and CLS
+    #' @return A chromosome
+    MutateChromosome <- function (chromosome) {
+        n_boxes <- length(chromosome$BPS) 
+        n_containers <- length(chromosome$CLS)
+
+        if (n_boxes <= 2) {
+            chromosome$BPS <- rev(chromosome$BPS)
+        } else {
+            replace_boxes_ind <- sample(1:n_boxes, 2)
+            chromosome$BPS[replace_boxes_ind] <- rev(chromosome$BPS[replace_boxes_ind])
+        }
+        
+        if (n_containers <= 2) {
+            chromosome$CLS <- rev(chromosome$CLS)            
+        } else {
+            replace_containers_ind <- sample(1:n_containers, 2)
+            chromosome$CLS[replace_containers_ind] <- rev(chromosome$CLS[replace_containers_ind])
+        }
+
+        return(chromosome)
+    }
+
+    population_size <- length(population)
+
+    # choose chromosomes for mutation with probability 'mutation_prob'
+    chromosomes_ind_to_mutate <- rbinom(n = population_size, size = 1, prob = mutation_prob)
+    
+    not_mutated_chromosomes <- population[chromosomes_ind_to_mutate == 0]
+    chromosomes_to_mutate <- population[chromosomes_ind_to_mutate == 1]
+
+    # Perform Chromosomes Mutation
+    mutated_chromosomes <- lapply(chromosomes_to_mutate, MutateChromosome)
+
+    new_population <- c(not_mutated_chromosomes, mutated_chromosomes)
+    return(new_population)
 }
